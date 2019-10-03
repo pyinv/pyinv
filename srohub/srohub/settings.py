0407deda45913b91e44583b334e327d0c681628c
Dan Trickey <dan@trickey.io>
2019-10-03 17:26:50 +0100

    Add a dashboard and basic layout

--- srohub/srohub/settings.py
+++ srohub/srohub/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'srohub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
