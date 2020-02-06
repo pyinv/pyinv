e897e3685ee9503af3af5b7db2a5590a1caa7957
Dan Trickey <dan@trickey.io>
2020-02-06 17:50:16 +0000

    Remove Access Code

--- srohub/access_codes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccessCodesConfig(AppConfig):
-    name = 'access_codes'
--- srohub/srohub/settings.py
+++ srohub/srohub/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # First Party
-    'access_codes',
     'accounts',
     'dashboard',
 
