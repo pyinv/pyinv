2e105b1bdb53fc9897d94563c04d59f4036fe9fd
Dan Trickey <dan@trickey.io>
2020-03-06 18:29:38 +0000

    Add Asset Manufacturer View

--- srohub/inventory/urls.py
+++ srohub/inventory/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', login_required(views.InventorySearchView.as_view()), name='index'),
     path('asset/<slug:slug>', login_required(views.AssetDisplayView.as_view()), name='asset_view'),
     path('model/<int:pk>', login_required(views.AssetModelDisplayView.as_view()), name='model_view'),
+    path('manufacturer/<int:pk>', login_required(views.AssetManufacturerDisplayView.as_view()), name='manufacturer_view'),
 ]
--- srohub/inventory/views.py
+++ srohub/inventory/views.py
@@ -2,7 +2,7 @@ from django.views.generic import DetailView, ListView
 from django.core.paginator import Paginator
 from django.db.models import Q
 
-from .models import Asset, AssetModel
+from .models import Asset, AssetModel, AssetManufacturer
 
 
 class InventorySearchView(ListView):
@@ -66,6 +66,25 @@ class AssetModelDisplayView(DetailView):
             page_num = 1
 
         assets = self.object.asset_set.order_by("-updated_at").all()
+        paginator = Paginator(assets, 20)
+        data['page_obj'] = paginator.get_page(page_num)
+        data['is_paginated'] = True
+        return data
+
+
+class AssetManufacturerDisplayView(DetailView):
+
+    model = AssetManufacturer
+    template_name = "inventory/manufacturer_view.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(object_list=object_list, **kwargs)
+
+        if 'page' in self.request.GET:
+            page_num = self.request.GET['page']
+        else:
+            page_num = 1
+        assets = self.object.assetmodel_set.order_by("-updated_at").all()
         paginator = Paginator(assets, 20)
         data['page_obj'] = paginator.get_page(page_num)
         data['is_paginated'] = True
