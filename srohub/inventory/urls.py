777b2be3dd7e200af8dc6f37f26de46ab0606df5
Dan Trickey <dan@trickey.io>
2020-03-06 12:44:48 +0000

    Add basic asset search function

--- srohub/inventory/urls.py
+++ srohub/inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 app_name = "inventory"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', login_required(views.InventorySearchView.as_view()), name='index'),
 ]
--- srohub/inventory/views.py
+++ srohub/inventory/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from django.db.models import Q
+
+from .models import Asset
 
 
-@login_required
-def index(request):
-    return render(request, "inventory/index.html")
+class InventorySearchView(ListView):
+
+    model = Asset
+    template_name = "inventory/search.html"
+    paginate_by = 15
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(object_list=object_list, **kwargs)
+        if 'query' in self.request.GET:
+            data['query'] = self.request.GET['query']
+        return data
+
+    def get_queryset(self):
+        if 'query' in self.request.GET:
+            return Asset.objects.filter(
+                Q(name__icontains=self.request.GET['query']) |
+                Q(notes__icontains=self.request.GET['query']) |
+                Q(asset_code__icontains=self.request.GET['query']),
+            )
+        else:
+            return Asset.objects.all()
