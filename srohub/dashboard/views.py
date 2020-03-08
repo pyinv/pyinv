cc6493b804eb5a5bd6124770f1d6fcf74a8dc07d
Dan Trickey <dan@trickey.io>
2020-03-08 21:15:36 +0000

    Redirect dashboard to inventory for now

--- srohub/dashboard/views.py
+++ srohub/dashboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def index(request):
-    return render(request, "dashboard/index.html")
+    # return render(request, "dashboard/index.html")
+    return redirect("inventory:index")
+
