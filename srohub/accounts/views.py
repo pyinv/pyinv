5382c65d502101197b4ebffc03e6cbb40025f48d
Dan Trickey <dan@trickey.io>
2020-03-10 22:50:26 +0000

    Remove unused import

--- srohub/accounts/views.py
+++ srohub/accounts/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http.response import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.views import generic
 
