b3abc5bba98baca8c8ea6238d05ce1fbdcd1f01c
Dan Trickey <dan@trickey.io>
2020-10-19 18:29:26 +0100

    Main seach is now usable

--- pyinv/inventory/views/misc.py
+++ pyinv/inventory/views/misc.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.views.generic import TemplateView
-from inventory.models import Asset
+from inventory.models import Asset, AssetModel, AssetManufacturer, Consumable, ConsumableModel
 
 
 class SearchView(LoginRequiredMixin, TemplateView):
@@ -11,6 +11,25 @@ class SearchView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         if "query" in self.request.GET:
-            data["query"] = self.request.GET["query"]
-            data["assets"] = Asset.get_search_queryset(self.request.GET["query"])[:10]
+            query = self.request.GET["query"]
+            data["query"] = query
+        else:
+            query = ""
+
+        data["assets_count"] = Asset.get_search_queryset(query).count()
+        data["assets"] = Asset.get_search_queryset(query)[:2]
+        data["models_count"] = AssetModel.get_search_queryset(query).count()
+        data["models"] = AssetModel.get_search_queryset(query)[:2]
+        data["manufacturers_count"] = AssetManufacturer.get_search_queryset(query).count()
+        data["manufacturers"] = AssetManufacturer.get_search_queryset(query)[:2]
+        data["consumables_count"] = Consumable.get_search_queryset(query).count()
+        data["consumables"] = Consumable.get_search_queryset(query)[:2]
+        # data["consumablemodels"] = ConsumableModel.get_search_queryset(self.request.GET["query"])
+
+        data["count_sum"] = sum([
+            data["assets_count"],
+            data["models_count"],
+            data["manufacturers_count"],
+            data["consumables_count"],
+        ])
         return data
